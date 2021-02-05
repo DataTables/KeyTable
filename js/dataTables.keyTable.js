83c79229dcb58ceb26476fbb89d75151c4f3cb33
Allan Jardine <allan@datatables.net>
2021-02-05 14:32:43 +0000

    KeyTable 2.6.0

--- js/dataTables.keyTable.js
+++ js/dataTables.keyTable.js
@@ -1,11 +1,11 @@
-/*! KeyTable 2.6.0-dev
+/*! KeyTable 2.6.0
  * ©2009-2021 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     KeyTable
  * @description Spreadsheet like keyboard navigation for DataTables
- * @version     2.6.0-dev
+ * @version     2.6.0
  * @file        dataTables.keyTable.js
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
@@ -1207,7 +1207,7 @@ KeyTable.defaults = {
 
 
 
-KeyTable.version = "2.6.0-dev";
+KeyTable.version = "2.6.0";
 
 
 $.fn.dataTable.KeyTable = KeyTable;
