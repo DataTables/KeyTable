80b148c4c9c0e5a1adaad5e64931475f0eeb0e03
Allan Jardine <allan@datatables.net>
2023-11-02 10:12:26 +0000

    Fix: Re-focusing calculation when triggered by a draw was wrong Fix: Improved integration with Scroller Fix: Improved interaction with server-side processing mode. Key events are now ignored while the table is in the middle of an async draw.

--- js/dataTables.keyTable.js
+++ js/dataTables.keyTable.js
@@ -38,6 +38,9 @@ var KeyTable = function (dt, opts) {
 		/** @type {DataTable.Api} DataTables' API instance */
 		dt: new DataTable.Api(dt),
 
+		/** Indicate when the DataTable is redrawing - take no action on key presses */
+		dtDrawing: false,
+
 		enable: true,
 
 		/** @type {bool} Flag for if a draw is triggered by focus */
@@ -165,8 +168,11 @@ $.extend(KeyTable.prototype, {
 
 		// Key events
 		$(document).on('keydown' + namespace, function (e) {
-			if (!editorBlock) {
+			if (!editorBlock && !that.s.dtDrawing) {
 				that._key(e);
+			}
+			else {
+				e.preventDefault();
 			}
 		});
 
@@ -291,8 +297,17 @@ $.extend(KeyTable.prototype, {
 			}
 		});
 
+		// When the table is about to do a draw we need to block key
+		// handling. This is only important for async draws - i.e.
+		// server-side processing.
+		dt.on('preDraw' + namespace + ' scroller-will-draw' + namespace, function (e) {
+			that.s.dtDrawing = true;
+		});
+
 		// Redraw - retain focus on the current cell
 		dt.on('draw' + namespace, function (e) {
+			that.s.dtDrawing = false;
+
 			that._tabInput();
 
 			if (that.s.focusDraw) {
@@ -304,9 +319,15 @@ $.extend(KeyTable.prototype, {
 			if (lastFocus) {
 				var relative = that.s.lastFocus.relative;
 				var info = dt.page.info();
-				var row = relative.row + info.start;
+				var row = relative.row;
 
 				if (info.recordsDisplay === 0) {
+					return;
+				}
+
+				// If the refocus is outside the current draw zone -
+				// don't attempt to refocus onto it
+				if (row < info.start || row > info.start + info.length) {
 					return;
 				}
 
@@ -761,11 +782,13 @@ $.extend(KeyTable.prototype, {
 		}
 
 		// Event and finish
+		var info = dt.page.info();
+
 		this.s.lastFocus = {
 			cell: cell,
 			node: cell.node(),
 			relative: {
-				row: dt.rows({ page: 'current' }).indexes().indexOf(cell.index().row),
+				row: info.start + dt.rows({ page: 'current' }).indexes().indexOf(cell.index().row),
 				column: cell.index().column
 			}
 		};
